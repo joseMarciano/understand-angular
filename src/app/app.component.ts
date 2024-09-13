972f52e7ba7075da281b709e9b816e170d5e98d2
jose.marciano@mheducation.com <jose.marciano@mheducation.com>
2024-09-13 20:12:27 -0300

    feat: :sparkles: adding a new component using selected user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,25 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {DUMMY_USERS} from "./dummy-users";
+import {TasksComponent} from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  protected readonly USERS = DUMMY_USERS
+  protected readonly USERS = DUMMY_USERS;
+  currentUserId = this.USERS[0].id;
+
+
+  get currentUser() {
+    return this.USERS.find(user => user.id === this.currentUserId);
+  }
 
   onSelectUser(id: string) {
-    console.log(id);
+    this.currentUserId = id
   }
 }
