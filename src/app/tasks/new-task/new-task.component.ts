e1c0103dc38033d95378bd9144c32e64e19119d1
jose.marciano@mheducation.com <jose.marciano@mheducation.com>
2024-09-17 20:56:05 -0300

    feat: :sparkles: use two way binding with signals

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,21 @@
-import {Component, output} from '@angular/core';
+import {Component, output, signal} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   onClose = output<void>();
+
+  title = signal("");
+  summary = signal("");
+  dueDate = signal("");
 
   onModalClose() {
     this.onClose.emit();
