10bc586e3a0fb8f969b448fd5a7fc81dcff511b2
jose.marciano@mheducation.com <jose.marciano@mheducation.com>
2024-09-28 11:32:20 -0300

    feat: using host elements like regular elements | interacting with host elements from inside components

--- components-and-templates-deep-dive/src/app/shared/control/control.component.ts
+++ components-and-templates-deep-dive/src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control', // add the class to app-control whenever is being used ex: <app-control class="control"/>
+  }
 })
 export class ControlComponent {
   label = input.required<string>()
