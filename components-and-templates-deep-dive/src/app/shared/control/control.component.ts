cb65fd0f3d67925d052d011edabce63352ed3925
jose.marciano@mheducation.com <jose.marciano@mheducation.com>
2024-09-28 12:04:18 -0300

    feat: accessing host elements programmatically

--- components-and-templates-deep-dive/src/app/shared/control/control.component.ts
+++ components-and-templates-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,10 +13,13 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   }
 })
 export class ControlComponent {
+  private readonly hostElement = inject(ElementRef);
+
   label = input.required<string>()
 
 
   onClick() {
     console.log('clicked');
+    console.log(this.hostElement)
   }
 }
